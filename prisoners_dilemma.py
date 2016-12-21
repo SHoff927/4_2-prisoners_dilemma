0334b64fd8df9a46cc7a550b204aa9ee886fde67
MMerz7 <megan.merz@students.sacs.k12.in.us>
2016-12-21 07:56:29 -0500

    commit1

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -161,6 +161,15 @@ def play_round(player1, player2, score1, score2, moves1, moves2):
         # Both players betray; get punishment.   
         score1 += PUNISHMENT
         score2 += PUNISHMENT     
+    elif actions == ' cb':
+        # Player 1 colludes, player 2 betrays; get severe, treat.
+        score1 += TREAT
+        score2 += SEVERE_PUNISHMENT
+        
+    elif actions == 'b c':
+        # Player 2 colludes, player 1 betrays; get severe, treat.
+        score1 += SEVERE_PUNISHMENT
+        score2 += TREAT
     else:
         # Both players get the "error score" if someone's code returns an improper action.
         score1 += ERROR
